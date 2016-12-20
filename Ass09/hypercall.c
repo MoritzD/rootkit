c91f65ad9b06d48f48e04a6dfd99b0bb7c63e71e
Moritz Dötterl <moritz.doetterl@tum.de>
2016-12-20 11:37:47 +0100

    Assignment 9 done

--- Ass09/hypercall.c
+++ Ass09/hypercall.c
@@ -1,16 +1,29 @@
+#include <linux/moduleloader.h>
+#include <linux/unistd.h>
+#include <linux/reboot.h>
+#include "../sysmap.h"		// for sys_call_table address this is Device spesific
 #include <asm/kvm_para.h>
-#include <asm/alternative.h>
-#define KVM_HYPERCALL \
-        ALTERNATIVE(".byte 0x0f,0x01,0xc1", ".byte 0x0f,0x01,0xd9", X86_FEATURE_VMMCALL)
 
-int main(int argc, char *argv[]) {
-	long ret;
-	unsigned int nr = 99;	// my hypercall
-	unsigned long p1 = 20; // arbitrary value
-//	ret = kvm_hypercall1(nr,p1);
-	asm volatile(KVM_HYPERCALL
-			: "=a"(ret)
-			: "a"(nr), "b"(p1)
-			: "memory");
-	return ret;
+#define DRIVER_AUTHOR "Moritz Dötterl <moritz.doetterl@tum.de>"
+#define DRIVER_DESC   "A sample kernel module: "
+
+unsigned long *sys_call_table = (unsigned long*) MAP_sys_call_table; // Address of the Syscall table from sysmap
+
+static int __init init_mod(void)
+{	
+	printk("inserting...\n");
+	kvm_hypercall1(99,20);
+	return 0;
 }
+
+static void __exit  exit_mod(void)
+{
+	printk("exiting...\n");
+} 
+
+module_init(init_mod);
+module_exit(exit_mod);
+
+MODULE_LICENSE("GPL"); 				/* Declare it as GPL License */
+MODULE_AUTHOR(DRIVER_AUTHOR);		/* Declare the Author        */
+MODULE_DESCRIPTION(DRIVER_DESC);	/* Short description         */
